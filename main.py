c1988e1a64440048db899451f3ef43006d0084ac
caiogrijo <0064700@sesi-es.org.br>
2026-08-05 09:09:27 -0300

    Menu Inicial

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ def menu():
     while True:
         print('----------|SISTEMA DE BIBLIOTECA|----------')
         print('1 - Cadastrar livro')
-        print('2 - ')
+        print('2 - Pegar livro emprestado')
+        print('3 - Devolver livro')
+        print('4 - Listar livros em nossa biblioteca')
+        print('5 - Buscar livros na biblioteca')
+        print('6 - Ordenar livros')
+        print('7 - Sair do Sistema')
+        
 menu()
