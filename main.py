7f312c8462a61ec5aaed7ee0b50ca7507c82bffc
caiogrijo <0064700@sesi-es.org.br>
2026-08-05 18:10:19 -0300

    Função de devolver concluída

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def cadastro_livro():
         escritor.writerow([titulo, autor, ano, isbn, status])
         print('Livro cadastrado com sucesso!')
 
+
 def emprestimo():
     isbn = input('Digite o ISBN do livro que gostaria de pegar emprestado: ')
     livros = []
@@ -53,7 +54,38 @@ def emprestimo():
         escritor.writerows(livros)
 
 
+def devolver():
+    isbn = input('Digite o ISBN do livro que gostaria de devolver: ')
+    livros = []
+    encontrado = False
+    
+    with open(ARQUIVO, "r", newline="", encoding="utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        cabecalho = next(leitor)
+        livros.append(cabecalho)
+    
+        for livro in leitor:
+            if livro[3] == isbn:
+                encontrado = True
+    
+                if livro[4] == "Emprestado":
+                    livro[4] = "Disponível"
+                    print("Livro devolvido com sucesso!")
+                else:
+                    print("Este livro não pode ser devolvido pois não está emprestado.")
+    
+            livros.append(livro)
+    
+    if not encontrado:
+        print("Livro não encontrado.")
+        return
+    
+    with open(ARQUIVO, "w", newline="", encoding="utf-8") as arquivo:
+        escritor = csv.writer(arquivo)
+        escritor.writerows(livros)
 
+def listar():
+    
 
 
 while True:
@@ -68,5 +100,9 @@ while True:
     opcao = int(input('Qual a opção deseja escolher?: '))
     if opcao == 1:
         cadastro_livro()
-    if opcao == 2:
+    elif opcao == 2:
         emprestimo()
+    elif opcao == 3:
+        devolver()
+    elif opcao == 4:
+        listar()
