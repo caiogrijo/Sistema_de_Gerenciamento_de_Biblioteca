82d9c3ebd3e5440fa6fd75fe9a2c7760b6ec2188
caiogrijo <0064700@sesi-es.org.br>
2026-08-05 17:17:25 -0300

    Criação de um arquivo csv e começo da função de cadastro de livros

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import csv
 import os
 
+ARQUIVO = "livros.csv"
+
+#Neste comando, estou pedindo para criar um arquivo "csv" caso não exista.
+if not os.path.exists(ARQUIVO):
+    with open(ARQUIVO, "w", newline="", encoding="utf-8") as arquivo:
+        escritor = csv.writer(arquivo)
+        escritor.writerow(["Título", "Autor", "Ano", "ISBN", "Status"])
+
 def cadastro_livro():
-    
+    print('Que livro gostaria de cadastrar?: ')
 
 
 
-def menu():
-    while True:
-        print('----------|SISTEMA DE BIBLIOTECA|----------')
-        print('1 - Cadastrar livro')
-        print('2 - Pegar livro emprestado')
-        print('3 - Devolver livro')
-        print('4 - Listar livros em nossa biblioteca')
-        print('5 - Buscar livros na biblioteca')
-        print('6 - Ordenar livros')
-        print('7 - Sair do Sistema')
-        opcao = int(input('Qual a opção deseja escolher?: '))
-        if opcao == 1:
-            cadastro()
+
+while True:
+    print('----------|SISTEMA DE BIBLIOTECA|----------')
+    print('1 - Cadastrar livro')
+    print('2 - Pegar livro emprestado')
+    print('3 - Devolver livro')
+    print('4 - Listar livros em nossa biblioteca')
+    print('5 - Buscar livros na biblioteca')
+    print('6 - Ordenar livros')
+    print('7 - Sair do Sistema')
+    opcao = int(input('Qual a opção deseja escolher?: '))
+    if opcao == 1:
+        cadastro_livro()
 menu()
