c0eb6c242337448b004cb0d9e7042b632c2dd90e
caiogrijo <0064700@sesi-es.org.br>
2026-08-07 18:34:42 -0300

    Alterações de dicionário no cadastro e empréstimo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import os
+livros = []
 
 ARQUIVO = "livros.csv"
 
@@ -9,7 +10,7 @@ if not os.path.exists(ARQUIVO):
         escritor = csv.writer(arquivo)
         escritor.writerow(["Título", "Autor", "Ano", "ISBN", "Status"])
 
-def cadastro_livro():
+def cadastro_livro(livros):
     titulo = input('Título do livro: ')
     autor = input('Autor do livro: ')
     ano = int(input('Ano de publicação: '))
@@ -17,40 +18,40 @@ def cadastro_livro():
     status = 'Disponível'
 
     #Processo de pegar as informações e por na tabela
-    with open(ARQUIVO, "a", newline="", encoding="utf-8") as arquivo:
-        escritor = csv.writer(arquivo)
-        escritor.writerow([titulo, autor, ano, isbn, status])
-        print('Livro cadastrado com sucesso!')
+    livro = {
+        'título': titulo ,
+        'autor': autor,
+        'ano': ano,
+        'isbn': isbn,
+        'status': status
+    }
+    livros.append(livro)
+    return livros
 
 
-def emprestimo():
+def emprestimo(livros):
     isbn = input('Digite o ISBN do livro que gostaria de pegar emprestado: ')
-    livros = [] # Lista que vai guardar TODOS os livros (incluindo o cabeçalho)
     encontrado = False
 
-    # Abre o arquivo em modo leitura ("r") para ler os dados atuais
-    with open(ARQUIVO, "r", newline="", encoding="utf-8") as arquivo:
-        leitor = csv.reader(arquivo)
-        cabecalho = next(leitor) # Lê a primeira linha do CSV (o cabeçalho: Título, Autor, Ano, ISBN, Status)
-        livros.append(cabecalho) # Adiciona o cabeçalho na lista, pois ele também precisa ser gravado para não perdermos os nomes das colunas
+    for livro in livros:
+        if livros['isbn'] == isbn: # livro[3] é a posição do ISBN dentro da linha
+            encontrado = True # marca que achamos o livro procurado
 
-        for livro in leitor:
-            if livro[3] == isbn: # livro[3] é a posição do ISBN dentro da linha
-                encontrado = True # marca que achamos o livro procurado
-
-                if livro[4] == "Disponível": # Verifica se o livro está disponível pra empréstimo
-                    livro[4] = "Emprestado"
-                    print("Livro emprestado com sucesso!")
-                else:
-                    print("Este livro já está emprestado.")
+            if livro['status'] == "Disponível": # Verifica se o livro está disponível pra empréstimo
+                livro['status'] = "Emprestado"
+                print("Livro emprestado com sucesso!")
+                return True
+            else:
+                print("Este livro já está emprestado.")
+                return False
 
             # Independente de ser o livro procurado ou não,
             # adiciona a linha na lista para não perder nenhum registro
-            livros.append(livro)
+        livros.append(livro)
 
     if not encontrado:
         print("Livro não encontrado.")
-        return  # sai da função sem reescrever o arquivo (não é necessário)
+        return False # sai da função sem reescrever o arquivo (não é necessário)
 
     # Reabre o arquivo, agora em modo escrita ("w"), o que apaga o conteúdo antigo
     with open(ARQUIVO, "w", newline="", encoding="utf-8") as arquivo:
@@ -175,9 +176,9 @@ while True:
     print('7 - Sair do Sistema')
     opcao = int(input('Qual a opção deseja escolher?: '))
     if opcao == 1:
-        cadastro_livro()
+        livros = cadastro_livro(livros)
     elif opcao == 2:
-        emprestimo()
+        emprestimo(livros)
     elif opcao == 3:
         devolver()
     elif opcao == 4:
